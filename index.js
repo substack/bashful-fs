4512ec2ada659cdc7bb07421e022a94aa9740aca
Thomas Levine <occurrence@thomaslevine.com>
2013-08-08 02:00:47 -0700

    docs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 module.exports = {}
 module.exports.exists = function(path, callback) {
   callback(true)
---
+++ readme.md
@@ -0,0 +1,3 @@
+bashful-fs
+============
+A filesystem for [bashful](https://github.com/substack/bashful)
