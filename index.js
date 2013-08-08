77f68b9af31e40d62668f478a59cebd15e4bbc7c
Thomas Levine <occurrence@thomaslevine.com>
2013-08-08 04:00:46 -0700

    attempt /dev implementation

--- index.js
+++ index.js
@@ -24,7 +24,31 @@ Writeable.prototype.write = Writeable.prototype.end = function(chunk, callback) 
   callback()
 }
 
+function PseudoWriteable () {}
+PseudoWriteable.prototype.on = PseudoWriteable.prototype.write = PseudoWriteable.prototype.end = function() {}
 
+function PseudoReadable () {}
+PseudoReadable.prototype.on = PseudoReadable.prototype.read = PseudoReadable.prototype.end = function() {}
+
+function DevZero () {}
+DevZero.prototype.on = DevZero.prototype.end = function() {}
+DevZero.prototype.read = function (size) {
+  var out = ''
+  for (var i = 0; i < size; i ++) {
+    out += String.fromCharCode(0)
+  }
+  return out
+}
+
+function DevUrandom () {}
+DevUrandom.prototype.on = DevUrandom.prototype.read = DevUrandom.prototype.end = function() {}
+DevUrandom.prototype.read = function (size) {
+  var out = ''
+  for (var i = 0; i < size; i ++) {
+    out += String.fromCharCode(Math.round(Math.random() * (Math.pow(2,16)-1)))
+  }
+  return out
+}
 
 
 module.exports = fs = {}
@@ -34,9 +58,21 @@ fs.exists = function(path, callback) {
 }
 
 fs.createReadStream = function(path /* no options */ ) {
-  return new Readable(path)
+  if (path === '/dev/null') {
+    return new PseudoReadable()
+  } else if (path === '/dev/urandom') {
+    return new DevUrandom()
+  } else if (path === '/dev/zero') {
+    return new DevZero()
+  } else {
+    return new Readable(path)
+  }
 }
 
 fs.createWriteStream = function(path /* no options */ ) {
-  return new Writeable(path)
+  if (path === '/dev/null' || path === '/dev/urandom' || path === '/dev/zero') {
+    return new PseudoWriteable()
+  } else {
+    return new Writeable(path)
+  }
 }
