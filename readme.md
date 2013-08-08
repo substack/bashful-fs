e76d6e334f9d39bb71bdab13fb7c7dd2e0666f5d
Thomas Levine <occurrence@thomaslevine.com>
2013-08-08 03:43:55 -0700

    /dev/zero tests

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 bashful-fs
 ============
-A filesystem for [bashful](https://github.com/substack/bashful)
+A filesystem for [bashful](https://github.com/substack/bashful),
+implementing `fs.exists`, `fs.createReadStream` and `fs.createWriteStream`
--- test/dev.js
+++ test/dev.js
@@ -20,3 +20,38 @@ test('/dev/null', function (t) {
   })
 })
 
+test('/dev/urandom', function (t) {
+  t.test('read', function(t) {
+    localStorage.removeItem('/dev/urandom')
+    var readable = fs.createReadStream('/dev/urandom')
+    t.equal(readable.read(24).length, 24)
+    t.end()
+  })
+  t.test('write', function(t) {
+    localStorage.removeItem('/dev/urandom')
+    var writeable = fs.createWriteStream('/dev/urandom')
+    writeable.write('saoehtsaoestnuhasotnehusantoehuatnoeuha', function(){
+      t.equal(localStorage.getItem('/dev/urandom'), null)
+      t.end()
+    })
+  })
+})
+
+test('/dev/zero', function (t) {
+  t.test('read', function(t) {
+    localStorage.removeItem('/dev/zero')
+    var zero = String.fromCharCode(0)
+    var readable = fs.createReadStream('/dev/zero')
+    t.equal(readable.read(3), zero + zero + zero)
+    t.end()
+  })
+  t.test('write', function(t) {
+    localStorage.removeItem('/dev/zero')
+    var writeable = fs.createWriteStream('/dev/zero')
+    writeable.write('saoehtsaoestnuhasotnehusantoehuatnoeuha', function(){
+      t.equal(localStorage.getItem('/dev/zero'), null)
+      t.end()
+    })
+  })
+})
+
