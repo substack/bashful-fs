479821ab1dff4ab276c82b014eec9548816acff6
Thomas Levine <occurrence@thomaslevine.com>
2013-08-08 02:54:54 -0700

    writer test stubs

--- test/createWriteStream.js
+++ test/createWriteStream.js
@@ -3,34 +3,57 @@ var localStorage = require('localStorage')
 
 var fs = require('../')
 
-test('Event "writeable"', function (t) {
+test('Event "drain"', function (t) {
   t.plan(1)
   localStorage.setItem('/quoin', 'abc')
   var writeable = fs.createWriteStream('/quoin')
-  writeable.on('readable', function() {
+  writeable.on('drain', function() {
     // This doesn't need to do anything, but it should not error.
     t.ok(true)
   })
 })
 
-test('Event "data"', function (t) {
+test('Event "finish"', function (t) {
   t.plan(1)
   localStorage.setItem('/wildebeest', '0123456789')
   var writeable = fs.createWriteStream('/wildebeest')
-  writeable.on('data', function(chunk) {
+  writeable.on('finish', function(chunk) {
     t.equal(chunk, '0123456789')
   })
 })
 
-test('Event "end"', function (t) {
+test('Event "unpipe"', function (t) {
   t.plan(1)
   localStorage.setItem('/hamster', '0123456789')
   var writeable = fs.createWriteStream('/hamster')
-  writeable.on('end', function(chunk) {
+  writeable.on('unpipe', function(chunk) {
     t.equal(chunk, '0123456789')
   })
 })
 
-test('Dummy functions', function(t) {
-  t.end()
+test('Event "pipe"', function (t) {
+  t.plan(1)
+  localStorage.setItem('/hamster', '0123456789')
+  var writeable = fs.createWriteStream('/hamster')
+  writeable.on('pipe', function(chunk) {
+    t.equal(chunk, '0123456789')
+  })
+})
+
+test('writeable.end', function (t) {
+  t.plan(1)
+  localStorage.setItem('/hamster', '0123456789')
+  var writeable = fs.createWriteStream('/hamster')
+  writeable.on('pipe', function(chunk) {
+    t.equal(chunk, '0123456789')
+  })
+})
+
+test('writeable.write', function (t) {
+  t.plan(1)
+  localStorage.setItem('/hamster', '0123456789')
+  var writeable = fs.createWriteStream('/hamster')
+  writeable.on('pipe', function(chunk) {
+    t.equal(chunk, '0123456789')
+  })
 })
